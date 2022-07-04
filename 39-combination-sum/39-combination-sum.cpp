6bc52540bfbb74e565d8d1d74ece662e4a2877b3
Vipul Kapoor <62841467+Vipul35@users.noreply.github.com>
2022-07-04 14:34:46 +0530

    Time: 211 ms (14.52%), Space: 69.1 MB (14.06%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sum(int idx,vector<int> &v,vector<int> candidates,int target,vector<vector<int>> &ans)
+    {
+        if(idx==candidates.size())
+        {
+            if(target==0)
+            {
+                ans.push_back(v);
+            }
+            return;
+        }
+        if(candidates[idx]<=target)
+        {
+            v.push_back(candidates[idx]);
+            sum(idx,v,candidates,target-candidates[idx],ans);
+            v.pop_back();
+        }
+        sum(idx+1,v,candidates,target,ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> v;
+        vector<vector<int>> ans;
+        sum(0,v,candidates,target,ans);
+        return ans;
+    }
+};
