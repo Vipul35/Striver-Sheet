1b3c37776c337576642396c855f494b05c9029bc
Vipul Kapoor <62841467+Vipul35@users.noreply.github.com>
2022-08-11 23:49:51 +0530

    Time: 12 ms (45.36%), Space: 8.9 MB (32.98%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums2.size();i++)
+        {
+            m[nums2[i]]=i;
+        }
+        vector<int> v;
+        for(int i=0;i<nums1.size();i++)
+        {
+            if(m.find(nums1[i])!=m.end())
+            {
+            int x=m[nums1[i]];
+                int ans=-1;
+            for(int j=x+1;j<nums2.size();j++)
+            {
+                if(nums2[j]>nums1[i])
+                {
+                    ans=nums2[j];
+                    break;
+                }
+            }
+                v.push_back(ans);
+            }
+        }
+        return v;
+    }
+};
