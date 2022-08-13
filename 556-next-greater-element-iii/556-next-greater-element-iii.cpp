b9afb2971d2816ac9828001fe27b49b2e8933123
Vipul Kapoor <62841467+Vipul35@users.noreply.github.com>
2022-08-13 14:53:45 +0530

    Time: 3 ms (37.79%), Space: 6.1 MB (7.01%) - LeetHub

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        vector<int> v;
+        int num=n;
+        while(n>0)
+        {
+            int digit=n%10;
+            v.push_back(digit);
+            n=n/10;
+        }
+        reverse(v.begin(),v.end());
+        next_permutation(v.begin(),v.end());
+        long ans=0;
+        for(int i=0;i<v.size();i++)
+        {
+            ans=ans*10;
+            ans+=v[i];
+        }
+        if(ans>long(INT_MAX) || ans<=num)
+        {
+            return -1;
+        }
+        return int(ans);
+    }
+};
