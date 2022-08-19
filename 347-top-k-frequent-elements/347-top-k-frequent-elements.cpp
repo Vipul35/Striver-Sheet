2dfca3b55b82c8ff41b559932fd72ca54c88020c
Vipul Kapoor <62841467+Vipul35@users.noreply.github.com>
2022-08-19 18:29:32 +0530

    Time: 12 ms (97.63%), Space: 13.8 MB (43.73%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -8,14 +8,10 @@ public:
         {
             m[nums[i]]++;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        priority_queue<pair<int,int>> pq;
         for(auto it:m)
         {
             pq.push({it.second,it.first});
-            if(pq.size()>k)
-            {
-                pq.pop();
-            }
         }
         for(int i=0;i<k;i++)
         {
