ca845ac1b16aae5f91c404350806100665804058
Vipul Kapoor <62841467+Vipul35@users.noreply.github.com>
2022-09-20 14:51:06 +0530

    Time: 37 ms (5.45%), Space: 10 MB (8.69%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -9,33 +9,51 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL)
+      //   if(head==NULL || head->next==NULL)
+      //   {
+      //       return NULL;
+      //   }
+      // ListNode*fast=head;
+      //   ListNode*slow=head;
+      //   bool cycle=false;
+      //   while(fast!=NULL && fast->next!=NULL)
+      //   {
+      //       fast=fast->next->next;
+      //       slow=slow->next;
+      //       if(fast==slow)
+      //       {
+      //           cycle=true;
+      //           break;
+      //       }
+      //   }
+      //   if(cycle==false)
+      //   {
+      //       return NULL;
+      //   }
+      //   slow=head;
+      //   while(slow!=fast)
+      //   {
+      //       slow=slow->next;
+      //       fast=fast->next;
+      //   }
+      //   return slow;
+        unordered_map<ListNode*,int> s;
+        ListNode*temp=head;
+        int i=0;
+        while(temp!=NULL)
         {
-            return NULL;
-        }
-      ListNode*fast=head;
-        ListNode*slow=head;
-        bool cycle=false;
-        while(fast!=NULL && fast->next!=NULL)
-        {
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast==slow)
+            if(s.find(temp)!=s.end())
             {
-                cycle=true;
+                return temp;
                 break;
             }
+            else
+            {
+                s[temp]=i;
+            }
+            temp=temp->next;
+            i++;
         }
-        if(cycle==false)
-        {
-            return NULL;
-        }
-        slow=head;
-        while(slow!=fast)
-        {
-            slow=slow->next;
-            fast=fast->next;
-        }
-        return slow;
+        return NULL;
     }
 };
